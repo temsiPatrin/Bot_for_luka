fd9f410e5ef6379786be5d543e5d02bdf6b1f0d4
temsi <patrik2207art@gmail.com>
2020-06-16 20:35:32 +0300

    обновил командер

--- commander.py
+++ commander.py
@@ -1,4 +1,5 @@
 class Commander:
+    last_msg = ""
 
     def reKeyboard(self,text:str):
 
@@ -6,6 +7,15 @@ class Commander:
             return "start_activity"
         if text == "В начало":
             return "start"
+        if text == "О компании":
+            return "start_activity"
+        if text == "Хочу сетку":
+            self.last_msg = "корпус"
+            return "color_canvas"
+        if self.last_msg == "корпус":
+            self.last_msg = ""
+            return "color_korpus"
+
         return "start"
 
 
@@ -14,4 +24,11 @@ class Commander:
             return "Я бот компании\n Выбери действие которое хочешь совершить"
         if text == "В начало":
             return "Нажми на кнопку начать"
+        if text == "О компании":
+            return "Иформация о компании..............."
+        if text == "Хочу сетку":
+            return "Выбери цвет полотна"
+        if self.last_msg == "корпус":
+            return "Выбери цвет корпуса"
+
         return "Нажми на кнопку начать"
